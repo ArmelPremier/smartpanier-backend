41858301d561472739d3644154042a68f4abe24f
ArmelPremier <houenouarmel3@gmail.com>
2026-04-19 13:36:27 +0100

    Backend stable (auth + produits + offres OK)

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 
-app = FastAPI()
+# 🔹 Import des routes
+from app.routes import produits, auth, offres
 
-# créer tables au démarrage
+app = FastAPI(
+    title="SmartPanier API",
+    description="API de comparaison de prix et optimisation de panier",
+    version="1.0.0"
+)
+
+# 🔹 Création des tables au démarrage
 @app.on_event("startup")
 def startup():
     Base.metadata.create_all(bind=engine)
 
+
+# 🔹 Route test
 @app.get("/")
 def home():
-    return {"message": "API + Supabase OK"}
+    return {"message": "API + Supabase OK 🚀"}
+
+
+# 🔥 🔥 🔥 AJOUT DES ROUTES
+
+app.include_router(produits.router)
+app.include_router(auth.router)
+app.include_router(offres.router)
